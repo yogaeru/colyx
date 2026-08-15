03d3de645d71ab59e5d00a4af1d106cb4b42c835
yogaeru <yogaadhip3@gmail.com>
2026-08-16 04:11:48 +0700

    fix: using specific version nitro and made koffi and sharp external

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,32 @@
 import { defineConfig } from "vite";
 import { devtools } from "@tanstack/devtools-vite";
-
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { nitro } from "nitro/vite";
-
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 const config = defineConfig(({ command }) => {
   return {
     resolve: { tsconfigPaths: true },
+    build: {
+      rolldownOptions: {
+        external: ["koffi", "sharp"],
+      },
+    },
     plugins: [
       devtools(),
       tailwindcss(),
-      ...(command === "build" ? [nitro()] : []),
-      tanstackStart(), 
+      ...(command === "build"
+        ? [
+            nitro(),
+          ]
+        : []),
+      tanstackStart(),
       viteReact(),
     ],
+    nitro: {
+      traceDeps: ["koffi", "sharp"]
+    },
   };
 });
 
